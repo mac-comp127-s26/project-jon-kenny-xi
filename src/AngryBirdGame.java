10251da52610b39ef3e09763cb203db60aa49acf
Xfang117 <fx456783@qq.com>
2026-04-26 18:11:20 -0500

    Birds class

--- src/AngryBirdGame.java
+++ src/AngryBirdGame.java
@@ -1,21 +1,28 @@
 import edu.macalester.graphics.CanvasWindow;
 
 public class AngryBirdGame {
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 800;
 
     private CanvasWindow canvas;
+    private Birds birds;
 
     public AngryBirdGame() {
+        double startX = CANVAS_WIDTH / 2;
+        double startY = CANVAS_HEIGHT * 0.7;
         canvas = new CanvasWindow("AngryBirdBattleGround", 2500, 1680);
-    }
-
-    private Birds addBirds() {
-        
+        birds = new Birds(canvas, startX, startY);
     }
 
     
+
+    
     public static void main(String[] args) {
+        new AngryBirdGame();
+
         
     }
     
 }
+
 
--- src/Birds.java
+++ src/Birds.java
@@ -1,13 +1,16 @@
+
+import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Image;
 
-
 public class Birds {
-    public Birds(CanvasWindow canvas, double centerX, double centerY){
-        shape = new Image(0, 0, "pcantrell-octopus.png").
+    private Image birdImage; 
 
+    public Birds(CanvasWindow canvas, double centerX, double centerY) {
+        birdImage = new Image(0, 0, "redbird.png");
+        birdImage.setMaxWidth(60);
+        
+        birdImage.setCenter(centerX, centerY);
+        canvas.add(birdImage);
     }
-
-
-    
 }
