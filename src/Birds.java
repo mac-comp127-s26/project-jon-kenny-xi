c1a07ffb2bd6e20f7d91f849b735c62cc2420a3b
jonathankim13 <jonkim2006@gmail.com>
2026-04-28 22:11:22 -0500

    Added Slingshot code, also edited some of the Birds and main code to work with the Slingshot

--- src/Birds.java
+++ src/Birds.java
@@ -6,35 +6,64 @@ import edu.macalester.graphics.Image;
 public class Birds {
     private Image birdImage; 
 
-    private double dx = 5; 
-    private double dy = -5; 
+    private double dx = 0; 
+    private double dy = 0; 
+    private double gravity = 0.15;
+    private boolean isFlying = false;
 
     private int pauseFrames = 0;
 
     public Birds(CanvasWindow canvas, double centerX, double centerY) {
         birdImage = new Image(0, 0, "redbird.png");
         birdImage.setMaxWidth(60);
-        
         birdImage.setCenter(centerX, centerY);
         canvas.add(birdImage);
     }
 
-    public void updatePosition(double canvasWidth, double canvasHeight) {
-        if (pauseFrames > 0) {
-            pauseFrames--;
-            return; 
+    public void setVelocity(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+        this.isFlying = true;
+    }
+
+    public void updateBirdPosition() {
+        if (isFlying) {
+            birdImage.moveBy(dx, dy);
+            dy += gravity; // Gravity makes the bird arc downward
         }
-
-        birdImage.moveBy(dx, dy);
-
-    }
-    public double getX() {
-        return birdImage.getX();
     }
 
-    public double getY() {
-        return birdImage.getY();
+    public void setPosition(double x, double y) {
+        birdImage.setCenter(x, y);
     }
+
+    public double getX() { 
+        return birdImage.getCenter().getX(); 
+    }
+    public double getY() { 
+        return birdImage.getCenter().getY(); 
+    }
+    public boolean isFlying() { 
+        return isFlying; 
+    }
+
+    // public void updatePosition(double canvasWidth, double canvasHeight) {
+    //     if (pauseFrames > 0) {
+    //         pauseFrames--;
+    //         return; 
+    //     }
+
+    //     birdImage.moveBy(dx, dy);
+
+    // }
+    
+    // public double getX() {
+    //     return birdImage.getX();
+    // }
+
+    // public double getY() {
+    //     return birdImage.getY();
+    // }
 
     public void reset(double centerX, double centerY) {
         birdImage.setCenter(centerX, centerY);
--- src/Slingshot.java
+++ src/Slingshot.java
@@ -1,15 +1,46 @@
+import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Line;
 import edu.macalester.graphics.Point;
+import java.awt.Color;
 
 public class Slingshot {
-    // just create the base of the slingshot, Rope.java will have the code to move the rope 
-    // at the end of the slingshot with the user's mouse
-    public static final double SLINGSHOT_LENGTH = 50;
-    private final Line line;
+    private Point anchor;
+    private Line rope;
+    private CanvasWindow canvas;
+    private double maxDrag = 100; // How far you can pull back
 
-    public Slingshot(double centerX, double centerY) {
-        line = new Line(centerX, centerY, 0, 0);
+    public Slingshot(CanvasWindow canvas, double x, double y) {
+        this.canvas = canvas;
+        this.anchor = new Point(x, y);
         
+        rope = new Line(x, y, x, y);
+        rope.setStrokeWidth(3);
+        rope.setStrokeColor(new Color(101, 67, 33));
+
+        Line frame = new Line(x, y, x, y + 60);
+        frame.setStrokeWidth(8);
+        canvas.add(frame);
     }
 
+    public void updateRope(Point birdPos) {
+        rope.setEndPosition(birdPos);
+
+        if (rope.getCanvas() == null) {
+            canvas.add(rope);
+        }
+    }
+
+    public void hideRope() {
+        if (rope.getCanvas() != null) {
+            canvas.remove(rope);
+        }
+    }
+
+    public Point getAnchor() {
+        return anchor;
+    }
+
+    public double getMaxDrag() {
+        return maxDrag;
+    }
 }
